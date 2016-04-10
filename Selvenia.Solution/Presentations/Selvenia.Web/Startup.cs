608b7f0bc82e4708172541b6b133b515876bedf2
Pandey <Anand_Pandey@amer.dell.com>
2016-04-09 19:03:55 -0500

    added initial admin area

--- Selvenia.Solution/Presentations/Selvenia.Web/Startup.cs
+++ Selvenia.Solution/Presentations/Selvenia.Web/Startup.cs
@@ -107,6 +107,10 @@ namespace Selvenia.Web
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "areaRoute",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
         }
 
